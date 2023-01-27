1cdff5a7fe43c53fe6261b9b8a4b8c6de45ccdc5
rsandeepreddy-98 <62950872+rsandeepreddy-98@users.noreply.github.com>
2023-01-27 20:02:35 +0530

    added new line

--- src/test/java/com/OrangeHRM/TestCases/NewFeature.java
+++ src/test/java/com/OrangeHRM/TestCases/NewFeature.java
@@ -3,5 +3,6 @@ package com.OrangeHRM.TestCases;
 public class NewFeature {
 public static void main(String[] args) {
 	System.out.println("For Demonstration purpose");
+	System.out.println("Changes reflecting");
 }
 }
